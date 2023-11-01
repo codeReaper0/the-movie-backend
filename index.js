274440a44aa7b79a4a5f0519afb965ffca377da0
codeReaper <boluwatifetella@gmail.com>
2023-11-01 16:25:29 +0100

    MODELS IN PROGRESS

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 mongoose
   .connect("mongodb://localhost/themovie")
@@ -12,6 +14,8 @@ mongoose
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}...`));
