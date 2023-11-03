ec6cf93faaada24f8f1a5939dfdca3401e01fa2e
codeReaper <boluwatifetella@gmail.com>
2023-11-03 17:08:19 +0100

    authentication in progress

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const {User} = require("../models/user");
+const router = express.Router();
+const Joi = require("joi");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.post("/", async (req, res) => {
+  const {error} = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({email: req.body.email});
+  if (!user) return res.status(400).send("Invalid login details");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (!validPassword) return res.status(400).send("Invalid login details");
+
+  const token = jwt.sign({_id: user._id}, "jwtPrivateKey");
+
+  res.status(200).send({token, message: "Login successful"});
+});
+
+function validate(req) {
+  const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+
+  return schema.validate(req);
+}
+
+module.exports = router;
